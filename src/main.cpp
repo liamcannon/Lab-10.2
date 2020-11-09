ecc5533cca4984702fe44729aad082a66cbd571f
Liam Cannon <liamcannon@Liams-MacBook-Pro.local>
2020-11-09 16:59:16 -0500

    changed for kml

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,48 @@ using namespace std;
 
 int main(int argc, char *argv[], char* env[])
 {
-    passwd *pws;
-    pws = getpwuid(geteuid());
-    cout << "user is: " << pws->pw_name << " userid is: "<< pws->pw_uid << endl;
+    int opt{};
+    bool kmlFlag{false};
+    bool logFlag{false};
+    string logValue;
+    string kmlValue;
+    bool optErr = true;
+    while((opt = getopt(argc, argv, "c:k:")) != EOF) {
+        switch(opt) {
+            case 'k':
+                kmlFlag = true;
+                kmlValue = atoi(optarg);
+                break;
+            case 'l':
+                logFlag = true;
+                logValue = optarg;
+                break;
+            default:
+                optErr = true;
+                break;
+        }
+    }
+    cout << "flags * countFlag: " << kmlFlag << " logFlag: " << logFlag << endl;
 
-    pid_t pid = getpid();
-    string programName = basename(argv[0]);
-    cout << "the pid of: " << programName << " is: " << pid << endl;
+    if(kmlFlag && logFlag) {
+        cout << "flags set" << endl;
+        if(logValue.empty() || kmlValue.empty()) {
+            optErr = true;
+            cout << "option arguments are not set" << endl;
+        }
+        else {
+            optErr = false;
+        }
+    }
+    else {
+        cout << "error - flags aren't set" << endl;
+        optErr = true;
+    }
 
+    if(optErr) {
+        return EXIT_FAILURE;
+    }
+    cout << "The count is: " << kmlValue << " and the logfile is: "<< logValue << endl;
+    cout << "optErr: " << optErr << endl;
     return EXIT_SUCCESS;
 }
